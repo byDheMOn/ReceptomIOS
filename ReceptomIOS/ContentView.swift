629c440a53c29a0a93e1a3334e5d5f0d6caf3352
Jorge Ordax <jorgeog1@hotmail.com>
2024-01-03 09:46:31 +0100

    Initial Commit

---
+++ ReceptomIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReceptomIOS
+//
+//  Created by Jorge Ordax on 3/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReceptomIOS/ReceptomIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceptomIOSApp.swift
+//  ReceptomIOS
+//
+//  Created by Jorge Ordax on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ReceptomIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
