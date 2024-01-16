dba4de0596b87d5c82b2028dfdd1e34e245d976f
Jorge Ordax <jorgeog1@hotmail.com>
2024-01-16 12:20:27 +0100

    16-01-2024

--- ReceptomIOS/Const/ApiConstants.swift
+++ ReceptomIOS/Const/ApiConstants.swift
@@ -7,10 +7,8 @@
 
 import Foundation
 struct ApiConstants {
+    // Add your API KEY
+    static let API_KEY = ""
     static let API_URL = "https://api.openai.com/v1/completions"
-    
-    // Estos ultimos dias OpenAi me ha eliminado 2 o 3 ApiKeys, si no funciona, avisame por teams.
-    static let API_KEY = "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"
-
     static let API_MODEL = "gpt-3.5-turbo-instruct"
 }
